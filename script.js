41a29ceeb819b10b487aef538d76f925976ff8f0
ryanX8716 <ryan.bora@escola.pr.gov.br>
2025-10-28 07:16:34 -0300

    script

--- script.js
+++ script.js
@@ -37,25 +37,43 @@ const perguntas = [
     enunciado:
       "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
     alternativas: [
-      "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-      "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
-    ],
+      {
+      texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+      afirmacao: "afirmação"
+      },
+      {
+      texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
+      afirmacao: "afirmação"
+      }
+    ]
   },
   {
     enunciado:
       "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
     alternativas: [
-      "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-      "Criar uma imagem utilizando um gerador de imagem de IA.",
-    ],
+      {
+      texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+      afirmacao: "afirmação"
+      },
+      {
+      texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+        afirmacao:"afirmação"
+      }
+      ]
   },
   {
     enunciado:
       "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
     alternativas: [
-      "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-      "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-    ],
+      {
+      texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+      afirmacao: "afirmação"
+      },
+      {
+      texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+        afirmacao:"afirmação"
+      }
+    ]
   },
 ];
 
@@ -73,11 +91,16 @@ function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function(){
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+function respostaSelecionada(opcaoSelecionada){
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal = afirmacoes;
+  atual++;
+  mostraPergunta();
+}
+
 mostraPergunta();
